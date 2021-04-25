2c269cbe2696097bc1c4b642cf5c4d5d6dfde0c4
FullGauss <mihaserg1234@gmail.com>
2021-04-25 17:14:36 +0700

    update ports

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
   SESSION_SECRET: process.env.SESSION_SECRET || 'super-secret',
   CORS_CONFIG: {
-    origin: process.env.CORS_ORIGIN || 'http://localhost:8080',
+    origin: process.env.CORS_ORIGIN || 'http://localhost:8081',
     credentials: true,
     methods: [ 'GET', 'POST', 'PUT' , 'UPDATE' , 'OPTIONS' ]
   }
